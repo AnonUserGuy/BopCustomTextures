4c7ab97aced4d4dcb5a5cb82accec74b4f025add
Luke Withrow <lukiewithrow@gmail.com>
2026-09-14 12:59:52 -0400

    decreased icon size also fixed RiqLoaderExtensions class to not be internal

--- BopCustomTextures/AccessExtensions/RiqLoaderExtensions.cs
+++ BopCustomTextures/AccessExtensions/RiqLoaderExtensions.cs
@@ -5,7 +5,7 @@ namespace BopCustomTextures.AccessExtensions;
 /// <summary>
 /// Extension methods for <see cref="RiqLoader"/> exposing private fields and methods.
 /// </summary>
-internal static class RiqLoaderExtensions
+public static class RiqLoaderExtensions
 {
     private static readonly TypedMethodInfo<RiqLoader> StartMixtapeMethod = new("StartMixtape", []);
     public static void StartMixtape(this RiqLoader obj) => StartMixtapeMethod.Invoke(obj);
